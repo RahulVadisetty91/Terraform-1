c3eb682f11df5b02121bf2d180ea3773c3218494
rahulvadisetty91 <rahulvadis@gmail.com>
2017-12-09 18:08:20 -0500

    Delete aws_variables.tf

--- aws_variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
-    default = "AKIAIQJDXWPBMP7VLP5Q"
-}
-variable "aws_secret_key" {
-    default = "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"
-}
-variable "aws_region" {
-    default = "us-east-2"
-}
